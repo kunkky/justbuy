28d176841b485a6e6f760a3d50ebfe1fe5bf53a6
Tolulope Olabode <tolulopepeter12@gmail.com>
2023-09-04 05:56:44 +0100

    homeSlider created
    Home Slider created and navbar updated

--- frontend/src/Components/HomeSections/HomeSlider.jsx
+++ frontend/src/Components/HomeSections/HomeSlider.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import ProductSlider from '../Assets/Frame.svg'
+import { Pagination, Navigation} from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 const HomeSlider = () => {
   return (
-      <div>HomeSlider for TeePee</div>
+    <Swiper className=' m-[3rem] w-[892px] h-[344px]'
+    modules={[Navigation, Pagination]}
+    spaceBetween={40}
+    slidesPerView={1}
+    navigation
+    pagination={{ clickable: true }}
+  >
+      <SwiperSlide><img src={ProductSlider} alt="Product Slider" />
+      </SwiperSlide>
+      <SwiperSlide><img src={ProductSlider} alt="Product Slider" />
+      </SwiperSlide>
+      <SwiperSlide> <img src={ProductSlider} alt="Product Slider" />
+      </SwiperSlide>
+    </Swiper>
   )
 }
 
