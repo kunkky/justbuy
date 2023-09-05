4e70f06b83cfa7403d99ef8bec7e717988f20b3f
kunkky <83945048+kunkky@users.noreply.github.com>
2023-09-05 07:11:32 -1200

    dac

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -5,9 +5,6 @@ import Cart from './Assets/Cart1.svg'
 import 'animate.css'
 const NavBar = () => {
   return (
-<<<<<<< HEAD
-    <div></div>
-=======
     <nav className=' flex justify-around pt-[1.5rem] h-[4rem] bg-white shadow-md'>
       <h1 className='font-bold text-[24px]'>Justbuy</h1>
       <ul className=' flex justify-evenly w-[20rem] text-[16px] pt-[0.5rem]'>
@@ -16,22 +13,17 @@ const NavBar = () => {
           ['Contact'],
           ['About'],
           ['Signup']
-        ].map(([title], index) => (
+        ].map(([title], index)=>(
           <li key={index} className='hover:border-b-2 hover:animate-[headShake_1s] hover:text-justbuy-purple border-justbuy-black cursor-pointer'> {title}</li>
-        {menuItems.map(([title])=>(
-          <li className='hover:border-b-2 hover:border-justbuy-purple hover:text-justbuy-purple hover:animate-[headShake_1s] border-justbuy-black cursor-pointer'> {title}</li>
-
         ))
         }
       </ul>
-
       <Search className=' w-[15rem] rounded-[4px]' placeholder='What are you looking for?' />
       <div className=' flex justify-center align-middle gap-[16px]'>
         <img src={Wishlist} className=' w-[32px] cursor-pointer' alt="Whishlists" />
         <img src={Cart} alt="Cart" className=' w-[32px] cursor-pointer' />
       </div>
     </nav>
->>>>>>> main
   )
 }
 
