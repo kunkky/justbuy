08727b6bf17131ba1e83ec9ab9619c85e9796d11
Tolulope Olabode <tolulopepeter12@gmail.com>
2023-09-09 12:01:40 +0100

    .

--- frontend/src/Components/HomeSections/HomeCategory.jsx
+++ frontend/src/Components/HomeSections/HomeCategory.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { Spin } from 'antd';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
@@ -12,91 +11,71 @@ import Camera from '../../Asset/Camera.svg';
 import Headphone from '../../Asset/Headphone.svg';
 import Gamepad from '../../Asset/Gamepad.svg';
 import BestSelling from "../../Components/HomeSections/BestSelling";
-// const categoryData = [
-//   {
-//     productName: 'Phones',
-//     productImg: phone,
-//     categoryUrl: '/phones', // Define the URL for the Phones category
-//   },
-//   {
-//     productName: 'Computers',
-//     productImg: computers,
-//     categoryUrl: '/computers', // Define the URL for the Computers category
-//   },
-//   {
-//     productName: 'Smart Watches',
-//     productImg: smartWatch,
-//     categoryUrl: '/smart-watches', // Define the URL for the Smart Watches category
-//   },
-//   {
-//     productName: 'Cameras',
-//     productImg: Camera,
-//     categoryUrl: '/cameras', // Define the URL for the Cameras category
-//   },
-//   {
-//     productName: 'Headphones',
-//     productImg: Headphone,
-//     categoryUrl: '/headphones', // Define the URL for the Headphones category
-//   },
-//   {
-//     productName: 'Gaming',
-//     productImg: Gamepad,
-//     categoryUrl: '/gaming', // Define the URL for the Gaming category
-//   },
-// ];
 
-// const HomeCategory = () => {
-//   const [products, setProducts] = useState([]);
-//   const [loading, setLoading] = useState(false);
+const categoryData = [
+  {
+    productName: 'Phones',
+    productImg: phone,
+    categoryUrl: '/phones', // Define the URL for the Phones category
+  },
+  {
+    productName: 'Computers',
+    productImg: computers,
+    categoryUrl: '/computers', // Define the URL for the Computers category
+  },
+  {
+    productName: 'Smart Watches',
+    productImg: smartWatch,
+    categoryUrl: '/smart-watches', // Define the URL for the Smart Watches category
+  },
+  {
+    productName: 'Cameras',
+    productImg: Camera,
+    categoryUrl: '/cameras', // Define the URL for the Cameras category
+  },
+  {
+    productName: 'Headphones',
+    productImg: Headphone,
+    categoryUrl: '/headphones', // Define the URL for the Headphones category
+  },
+  {
+    productName: 'Gaming',
+    productImg: Gamepad,
+    categoryUrl: '/gaming', // Define the URL for the Gaming category
+  },
+];
 
-//   return (
-//     <div className="h-screen">
-//       {loading ? (
-//         <Spin size="large" className="flex items-center justify-center" />
-//       ) : (
-//         <>
-//           <div className="flex flex-col md:flex-row gap-2 md:gap-2 items-center text-[#660B7F] px-4 md:px-8">
-//             <img src={rec} alt="" className="md:mr-2" />
-//             <h1 className="text-xl sm:text-xl">Categories</h1>
-//           </div>
+const HomeCategory = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-//           <div className="flex items-center justify-between px-4 md:px-8">
-//             <div>
-//               <h1 className="text-xl sm:text-2xl font-bold">Browse By Category</h1>
-//             </div>
-//             <div className="flex gap-3 pr-3">
-//               <img src={leftArrow} alt="" />
-//               <img src={rightArrow} alt="" />
-//             </div>
-//           </div>
+  return (
+    <div className="h-screen">
+      {loading ? (
+        <Spin size="large" className="flex items-center justify-center" />
+      ) : (
+        <>
+          <div className="flex flex-col md:flex-row gap-2 md:gap-2 items-center text-[#660B7F] px-4 md:px-8">
+            <img src={rec} alt="" className="md:mr-2" />
+            <h1 className="text-xl sm:text-xl">Categories</h1>
+          </div>
 
-//           <div className="flex flex-wrap gap-2 md:gap-4 lg:gap-6 justify-center items-center pt-8 px-2 md:px-8">
-//             {categoryData.map((item, index) => (
-//               <Link target='blank'
-//                 key={index}
-//                 to={item.categoryUrl} // Set the URL for each category
-//                 className="bg-opacity-70 bg-gray-900  hover:bg-[#660B7F] hover:text-white transition duration-300 shadow-sm rounded border border-[#660B7F] w-[10rem] md:w-[12rem] flex flex-col items-center cursor-pointer mb-2 hover:scale-105 ease-in-out hover:shadow-lg"
-//               >
-//                 <img src={item.productImg} alt={item.productName} className="max-h-40 md:max-h-48" />
-//                 <div className="text-center text-sm sm:text-base md:text-lg py-2">
-//                   {item.productName}
-//                 </div>
-//               </Link>
-//             ))}
-//           </div>
-//         </>
-//       )}
-//       <BestSelling />
-//     </div>
-//   );
-// };
+          <div className="flex items-center justify-between px-4 md:px-8">
+            <div>
+              <h1 className="text-xl sm:text-2xl font-bold">Browse By Category</h1>
+            </div>
+            <div className="flex gap-3 pr-3">
+              <img src={leftArrow} alt="" />
+              <img src={rightArrow} alt="" />
+            </div>
+          </div>
 
           <div className="flex flex-wrap gap-2 md:gap-4 lg:gap-6 justify-center items-center pt-8 px-2 md:px-8">
             {categoryData.map((item, index) => (
               <Link target='blank'
                 key={index}
                 to={item.categoryUrl} // Set the URL for each category
-                className="bg-opacity-70 bg-gray-900  hover:bg-[#660B7F] hover:text-white transition duration-300 shadow-sm rounded border border-[#660B7F] w-[10rem] md:w-[12rem] flex flex-col items-center cursor-pointer mb-2 hover:scale-105 ease-in-out hover:shadow-lg"
+                className="bg-opacity-70 bg-[#F5F5F5]  hover:bg-[#660B7F] hover:text-white transition duration-300 shadow-sm rounded border border-[#660B7F] w-[10rem] md:w-[12rem] flex flex-col items-center cursor-pointer mb-2 hover:scale-105 ease-in-out hover:shadow-lg"
               >
                 <img src={item.productImg} alt={item.productName} className="max-h-40 md:max-h-48" />
                 <div className="text-center text-sm sm:text-base md:text-lg py-2">
@@ -107,6 +86,7 @@ import BestSelling from "../../Components/HomeSections/BestSelling";
           </div>
         </>
       )}
+      <hr  className=' mt-20  ml-10 mr-10 h-2 rounded-md'/>
       <BestSelling />
     </div>
   );
--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -28,29 +28,42 @@ const NavBar = () => {
 
 
   return (
-<<<<<<< HEAD
-    <div></div>
-=======
-    <nav className=' flex justify-around pt-[1.5rem] h-[4rem] bg-white shadow-md'>
-      <h1 className='font-bold text-[24px]'>Justbuy</h1>
-      <ul className=' flex justify-evenly w-[20rem] text-[16px] pt-[0.5rem]'>
-        {[
-          ['Home'],
-          ['Contact'],
-          ['About'],
-          ['Signup']
-        ].map(([title], index)=>(
-          <li key={index} className='hover:border-b-2 hover:animate-[headShake_1s] hover:text-justbuy-purple border-justbuy-black cursor-pointer'> {title}</li>
-        ))
-        }
-      </ul>
-      <Search className=' w-[15rem] rounded-[4px]' placeholder='What are you looking for?' />
-      <div className=' flex justify-center align-middle gap-[16px]'>
-        <img src={Wishlist} className=' w-[32px] cursor-pointer' alt="Whishlists" />
-        <img src={Cart} alt="Cart" className=' w-[32px] cursor-pointer' />
-      </div>
-    </nav>
->>>>>>> main
+    <div>
+      <nav className=' p-[0.4rem] flex justify-around bg-justbuy-black text-[0.8rem] sm:p-[0.5rem]'>
+        <p className=' text-white pl-[11rem] md:pl-0 sm:max-w-[15rem]'> Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! <span className='underline cursor-pointer pl-[1rem]'>ShopNow</span></p>
+        <select name="countries" id="" className=' bg-justbuy-black text-white cursor-pointer'>
+          <option value="English">English</option>
+          <option value="France">French</option>
+        </select>
+      </nav>
+      <nav className='flex justify-around pt-[1.5rem] h-[4rem] bg-white shadow-md'>
+        <img src={Hamburger} onClick={menuHandler} alt="Menu" className='xl:hidden w-[1.5rem]' />
+        <h1 className='font-bold text-[24px] '>Justbuy</h1>
+        <ul className=' flex justify-evenly w-[20rem] md:hidden text-[16px] pt-[0.5rem]'>
+          {[
+            ['Home', '/'],
+            ['Contact', ''],
+            ['About'],
+            ['Signup', '/signUp']
+          ].map(([title, url], index) => (
+            <li key={index} className='hover:border-b-2 hover:animate-[headShake_1s] hover:text-justbuy-purple border-justbuy-black cursor-pointer'><Link to={url}>{title}</Link></li>
+          ))}
+        </ul>
+
+        <Search className=' md:hidden w-[15rem] rounded-[4px]' placeholder='What are you looking for?' />
+        <div className=' flex justify-center align-middle gap-[16px]'>
+          <img src={Wishlist} className='active:bg-justbuy-brown rounded-2xl active:animate-ping w-[32px] bg cursor-pointer md:hidden' alt="Whishlists" />
+          {navIcon && (
+            <div className=' flex gap-[1rem]'>
+              <img src={Cart} alt="Cart" className=' w-[32px] cursor-pointer active:animate-spin' />
+              <img src={SearchIcon} alt="Search" className=' animate-[bounceIn_1s] xl:hidden w-[2rem]' onClick={searhHandler} />
+            </div>
+          )}
+          {showSearch && (<Search className=' animate-[bounceIn_1s]' onSearch={onSearchHandler} placeholder='What are you looking for?' />)}
+        </div>
+      </nav>
+      <Menu showMenu={showMenu} menuHandler={menuHandler} />
+    </div>
   )
 }
 
